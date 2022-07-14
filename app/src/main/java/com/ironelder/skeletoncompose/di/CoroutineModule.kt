e613a2b3a315de5dde1498b6f0a9d1859c512d79
ironelder <ironelder@gmail.com>
2022-07-14 20:06:26 +0900

    model name 변경

--- app/src/main/java/com/ironelder/skeletoncompose/di/CoroutineModule.kt
+++ app/src/main/java/com/ironelder/skeletoncompose/di/CoroutineModule.kt
@@ -14,6 +14,6 @@ class CoroutineModule {
     fun providesIoDispatcher() = Dispatchers.IO
 
     @Provides
-    fun providesMainDispatcher() = Dispatchers.Main.immediate
+    fun providesMainImmediateDispatcher() = Dispatchers.Main.immediate
 
 }
