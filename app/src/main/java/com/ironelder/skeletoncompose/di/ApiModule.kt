bc18e10aa8de1f4fd531e5cdb27a07e70bfb8716
jhjang <jhjang@10.11.52.125>
2022-07-15 17:59:29 +0900

    book api 레트로핏 추가

--- app/src/main/java/com/ironelder/skeletoncompose/di/ApiModule.kt
+++ app/src/main/java/com/ironelder/skeletoncompose/di/ApiModule.kt
@@ -3,6 +3,7 @@ package com.ironelder.skeletoncompose.di
 import com.ironelder.skeletoncompose.data.CommonApi
 import com.ironelder.skeletoncompose.data.CommonRepository
 import com.ironelder.skeletoncompose.data.CommonRepositoryImpl
+import com.ironelder.skeletoncompose.data.SearchBookApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -19,7 +20,13 @@ class ApiModule {
 
     @Singleton
     @Provides
-    fun provideCommonApi(@Named("retrofit_skeleton") retrofit: Retrofit): CommonApi = retrofit.create()
+    fun provideCommonApi(@Named("retrofit_skeleton") retrofit: Retrofit): CommonApi =
+        retrofit.create()
+
+    @Singleton
+    @Provides
+    fun provideSearchBookApi(@Named("retrofit_search_book") retrofit: Retrofit): SearchBookApi =
+        retrofit.create()
 
     @Singleton
     @Provides
