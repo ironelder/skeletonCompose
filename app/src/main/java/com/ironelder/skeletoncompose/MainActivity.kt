7ff22db829355e27d343d905bd4194d378d0138e
jhjang <jhjang@10.11.52.125>
2022-07-15 15:30:23 +0900

    코드 정리

--- app/src/main/java/com/ironelder/skeletoncompose/MainActivity.kt
+++ app/src/main/java/com/ironelder/skeletoncompose/MainActivity.kt
@@ -29,29 +29,5 @@ class MainActivity : ComponentActivity() {
                 NavigationScreen(navController = rememberNavController())
             }
         }
-//        setContent {
-//            SkeletonComposeTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colors.background
-//                ) {
-//                    Greeting("Android")
-//                }
-//            }
-//        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SkeletonComposeTheme {
-        Greeting("Android")
     }
 }
